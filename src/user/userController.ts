e3757109109a003fa2295d35376a0274a8ab7c03
SarthakGagapalliwar <sarthakgagapalliwar07@gmail.com>
2025-10-07 14:23:55 +0530

    bcrypt is added to password

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -1,5 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 import createHttpError from "http-errors";
+import userModel from "./userModel";
+import bcrypt from "bcrypt"
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
   try {
@@ -13,7 +15,30 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
       return next(error);
     }
 
+    //Database call
+    const user=await userModel.findOne({
+    //   email: email if key value are are same use simple
+        email
+    })
+    if(user){
+        const error=createHttpError(400,"User alreday exits with this email");
+        return next(error);
+    }
+    //password=>hash
+
+    const hashedPassword = await bcrypt.hash(password,10);
+
+    const newUser =await userModel.create({
+        name,
+        email,
+        password:hashedPassword,
+    })
     // Process (e.g., save to DB here)
+
+    res.json({id:newUser._id});
+
+    //Token genration JWT
+    
 
     // Response
     return res.status(201).json({
